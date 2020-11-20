32f8004287fa27d44d963546ee76ce67e9ca497b
MarkovOA <markov-oa@mail.ri>
2020-11-20 15:26:13 +0500

    Try play with rivescript

--- VkBot_riveSC/main.py
+++ VkBot_riveSC/main.py
@@ -1,22 +1,30 @@
 from rivescript import RiveScript
+import json
 
-rs = RiveScript()
+# def CreateTemplate():
+#     Template = '''+ [*] меню [*]
+# - {"msg":"Теперь Вы в разделе Меню!",
+#     ^"photo":"test_photo",
+#     ^"keyboard":"test_button"}'''
+#     with open('Template.rive', 'w', encoding='utf-8') as file:
+#         file.write(Template)
+# CreateTemplate()
+rs = RiveScript(utf8=True)
 rs.load_directory("./eg/brain")
 rs.sort_replies()
 
-print("""This is a bare minimal example for how to write your own RiveScript bot!
-For a more full-fledged example, try running: `python rivescript brain`
-This will run the built-in Interactive Mode of the RiveScript library. It has
-some more advanced features like supporting JSON for communication with the
-bot. See `python rivescript --help` for more info.
-example.py is just a simple script that loads the RiveScript documents from
-the 'brain/' folder, and lets you chat with the bot.
-Type /quit when you're done to exit this example.
-""")
+
 
 while True:
     msg = input("You> ")
-    if msg == '/quit':
+    if msg == "/quit":
         quit()
     reply = rs.reply("localuser", msg)
-    print("Bot>", reply)
+    # print("Bot>", reply)
+    try:
+        reply = json.loads(reply)
+        print(f'msg:{reply.get("msg", "")}')
+        print(f'photo:{reply.get("photo", "")}')
+        print(f'keyboard:{reply.get("keyboard", "")}')
+    except:
+        print(f'msg:{reply}')
