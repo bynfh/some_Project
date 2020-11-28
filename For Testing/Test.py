cda68e7961ea1c1966e34e5ff5e5f229d7862b59
bynfh <markov-oa@mail.ru>
2020-11-28 10:03:16 +0500

    Test

--- For Testing/Test.py
+++ For Testing/Test.py
@@ -2,7 +2,9 @@ def fact(n):
     if n <= 1:
         return 1
     else:
-        print(n-1, n)
-        return fact(n-1) * n
+        print(n - 1, n)
+        return fact(n - 1) * n
 
-print (fact(5))
+
+print(fact(5))
+print(fact(10))
